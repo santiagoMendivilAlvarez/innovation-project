4b6dbff08116c2212bfdbb5e083929813c7987fc
Leslie Lucero Franco Calleros <al223053@alumnos.uacj.mx>
2025-09-26 19:13:54 -0600

    Resolucion de conflictos

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,7 +1,7 @@
 """
 URLs for authentication app.
 """
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -20,5 +20,6 @@ urlpatterns = [
     #API endpoints
     path('api/send-verification-code/', views.send_verification_code, name='send_verification_code'),
     path('api/user-data/', views.user_data_api, name='user_data_api'),
-    path('api/update-intereses/', views.update_intereses_api, name='update_intereses_api')
+    path('api/update-intereses/', views.update_intereses_api, name='update_intereses_api'),
+    path('libros/', include("libros.urls")),
 ]
