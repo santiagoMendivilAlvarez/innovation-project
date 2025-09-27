202aae0a8ff9abeb3fbcc58432b931dc7778c568
Leslie Lucero Franco Calleros <al223053@alumnos.uacj.mx>
2025-09-26 19:01:05 -0600

    Se implemento los elementos para cambiar de contrasenia con autenticacion, ademas de que se arreglaron la visualizacion de mensajes

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -11,10 +11,14 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    
     path('confirm_email/', views.confirm_email_view, name='confirm_email'),
-    
+
+    # Password reset
+    path('forgot-password/', views.forgot_password_view, name='forgot_password'),
+    path('verify-reset-code/', views.verify_reset_code_view, name='verify_reset_code'),
+    path('reset-password/', views.reset_password_view, name='reset_password'),
+    #API endpoints
     path('api/send-verification-code/', views.send_verification_code, name='send_verification_code'),
     path('api/user-data/', views.user_data_api, name='user_data_api'),
-    path('api/update-intereses/', views.update_intereses_api, name='update_intereses_api'),
+    path('api/update-intereses/', views.update_intereses_api, name='update_intereses_api')
 ]
