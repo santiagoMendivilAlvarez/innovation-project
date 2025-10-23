357aa77665d3ca27706320a18cc21f4f0bcad1d5
Santiago <Santiago_Alvarez@wiwynn.com>
2025-10-23 11:18:23 -0600

    [MODIFIED] Documentation in authentication app

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,7 +1,7 @@
 """
 URLs for authentication app.
 """
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -13,10 +13,8 @@ urlpatterns = [
     path('verify-reset-code/', views.verify_reset_code_view,
          name='verify_reset_code'),
     path('reset-password/', views.reset_password_view, name='reset_password'),
-    # API endpoints
     path('api/send-verification-code/', views.send_verification_code,
-         name='send_verification_code'),
+          name='send_verification_code'),
     path('api/user-data/', views.user_data_api, name='user_data_api'),
-    path('api/update-intereses/', views.update_intereses_api,
-         name='update_intereses_api'),
+    path('api/update-intereses/', views.update_intereses_api, name='update_intereses_api'),
 ]
